44b15f359dfbe05caf9e3d4481194cd90bf2c13f
Alex Jacks and Brett New <brett@>
2016-07-05 13:29:50 -0700

    all specs passing thanks to changing data class to integer

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,24 +1,22 @@
 class Triangle
   define_method(:initialize) do |a,b,c|
-    @a = a
-    @b = b
-    @c = c
+    @a = a.to_i
+    @b = b.to_i
+    @c = c.to_i
   end
 
-  define_method(:triangle?) do
-    @a.+(@b).<=(@c) && @c.+(@b).<=(@a) && @a.+(@c).<=(@b)
-    false
-  end
-
-  define_method(:triangle_type) do
-    if @a == @b && @b == @c
-      @triangle_name = "equilateral"
-    elsif
-      (@a == @b && @a != @c) || (@b == @c && @b != @a) || (@c == @a && @c != @b)
-      @triangle_name = "isosceles"
+  define_method(:triangle) do
+    if !(@a.+(@b).<=(@c) || @c.+(@b).<=(@a) || @a.+(@c).<=(@b))
+      if (@a == @b && @b == @c)
+        @triangle_name = "equilateral"
+      elsif (@a == @b) || (@b == @c) || (@c == @a)
+        @triangle_name = "isosceles"
+      else
+        @triangle_name = "scalene"
+      end
     else
-      @triangle_name = "scalene"
+      @triangle_name = "not a triangle"
     end
   end
-    @triangle_name
+  @triangle_name
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,24 +2,22 @@ require 'rspec'
 require 'triangle'
 
 describe Triangle do
-  describe("#triangle?") do
-    it('returns false if it is not a triangle') do
-      test_triangle = Triangle.new('1', '1', '4')
-      expect(test_triangle.triangle?()).to(eq(false))
+  describe('#triangle') do
+    it('returns "not a triangle" if it is not a triangle') do
+      test_triangle = Triangle.new('1', '2', '7')
+      expect(test_triangle.triangle()).to(eq("not a triangle"))
     end
-  end
-  describe('#triangle_type') do
-    it('returns equilateral when all three side values are equal') do
+    it('returns "equilateral" when all three side values are equal') do
       test_triangle = Triangle.new('3', '3', '3')
-      expect(test_triangle.triangle_type()).to(eq("equilateral"))
+      expect(test_triangle.triangle()).to(eq("equilateral"))
     end
-    it('returns isosceles when only 2 side values are equal') do
+    it('returns "isosceles" when 2 side values are equal') do
       test_triangle = Triangle.new('8', '8', '15')
-      expect(test_triangle.triangle_type()).to(eq("isosceles"))
+      expect(test_triangle.triangle()).to(eq("isosceles"))
     end
-    it('returns scalene when only no side values are equal') do
-      test_triangle = Triangle.new('1', '8', '15')
-      expect(test_triangle.triangle_type()).to(eq("scalene"))
+    it('returns "scalene" when no side values are equal') do
+      test_triangle = Triangle.new('3', '4', '5')
+      expect(test_triangle.triangle()).to(eq("scalene"))
     end
 
   end
