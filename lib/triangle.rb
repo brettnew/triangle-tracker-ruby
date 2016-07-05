4da0c1e7f0dec1a66dd2fce9cffdad2bbefc660f
Alex Jacks and Brett New <brett@>
2016-07-05 10:47:00 -0700

    adds specs for isoceles and scalene conditions, with successful tests

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,19 @@ class Triangle
   end
 
   define_method(:triangle?) do
-    @a == @b && @b == @c
+    @a.+(@b).<=(@c) && @c.+(@b).<=(@a) && @a.+(@c).<=(@b)
+    false
   end
+
+  define_method(:triangle_type) do
+    if @a == @b && @b == @c
+      @triangle_name = "equilateral"
+    elsif
+      (@a == @b && @a != @c) || (@b == @c && @b != @a) || (@c == @a && @c != @b)
+      @triangle_name = "isosceles"
+    else
+      @triangle_name = "scalene"
+    end
+  end
+    @triangle_name
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,18 +2,25 @@ require 'rspec'
 require 'triangle'
 
 describe Triangle do
-  describe('#triangle?') do
-    # it('returns the first side') do
-    #   test_triangle = Triangle.new('3', '4', '5')
-    #   expect(test_triangle.a).to(eq(3))
-    # end
-    it('returns false when all three side values are not equal') do
-      test_triangle = Triangle.new('3', '3', '4')
+  describe("#triangle?") do
+    it('returns false if it is not a triangle') do
+      test_triangle = Triangle.new('1', '1', '4')
       expect(test_triangle.triangle?()).to(eq(false))
     end
-    it('returns true when all three side values are equal') do
+  end
+  describe('#triangle_type') do
+    it('returns equilateral when all three side values are equal') do
       test_triangle = Triangle.new('3', '3', '3')
-      expect(test_triangle.triangle?()).to(eq(true))
+      expect(test_triangle.triangle_type()).to(eq("equilateral"))
     end
+    it('returns isosceles when only 2 side values are equal') do
+      test_triangle = Triangle.new('8', '8', '15')
+      expect(test_triangle.triangle_type()).to(eq("isosceles"))
+    end
+    it('returns scalene when only no side values are equal') do
+      test_triangle = Triangle.new('1', '8', '15')
+      expect(test_triangle.triangle_type()).to(eq("scalene"))
+    end
+
   end
 end
